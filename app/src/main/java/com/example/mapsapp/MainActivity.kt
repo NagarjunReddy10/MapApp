39879a9592ddc70f88ddb28e44c501ca5d090beb
NagarjunReddy10 <pashamnandu.12@gmail.com>
2026-02-24 17:27:18 +0530

    modified

--- app/src/main/java/com/example/mapsapp/MainActivity.kt
+++ app/src/main/java/com/example/mapsapp/MainActivity.kt
@@ -4,19 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.mapsapp.data.LocationRepository
 import com.example.mapsapp.navigation.AppNavigation
-import com.example.mapsapp.ui.screen.DropScreen
-import com.example.mapsapp.ui.screen.MapScreen
-import com.example.mapsapp.viewmodel.MapViewModel
-import com.example.mapsapp.viewmodel.MapViewModelFactory
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

a5ff63a6b3b38c26adcc4fde7f89639cae0a5d41
NagarjunReddy10 <pashamnandu.12@gmail.com>
2026-02-24 17:29:48 +0530

    modified

--- app/src/main/java/com/example/mapsapp/ui/screen/MapScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screen/MapScreen.kt
@@ -13,9 +13,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp

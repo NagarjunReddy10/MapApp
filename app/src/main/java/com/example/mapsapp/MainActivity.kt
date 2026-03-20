df7832d088a19ac3c7164905f76a11a76390bdd1
NagarjunReddy10 <pashamnandu.12@gmail.com>
2026-03-20 18:34:17 +0530

    changes done

--- app/src/main/java/com/example/mapsapp/MainActivity.kt
+++ app/src/main/java/com/example/mapsapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.mapsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.mapsapp.navigation.AppNavigation
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AppNavigation()
         }
